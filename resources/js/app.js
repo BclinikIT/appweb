c7cd86165d030fa924889648a3e091545868e7f4
GPAlexander <gpfordevoloper@gmail.com>
2024-07-22 17:47:16 -0600

    Deploy en railway solucion

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import VueApexCharts from 'vue3-apexcharts';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),

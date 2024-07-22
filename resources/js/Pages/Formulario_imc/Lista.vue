9ff0a77eb56adb4e5bcf430aaa2116ac6b849f3e
GPAlexander <gpfordevoloper@gmail.com>
2024-07-22 13:40:39 -0600

    Merge branch 'temp-branch'

--- resources/js/Pages/Formulario_imc/Lista.vue
+++ resources/js/Pages/Formulario_imc/Lista.vue
@@ -238,7 +238,6 @@ export default {
                     </div>
 
 
-
                     <div class="w-4/5 pl-4">
                         <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
                             <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">

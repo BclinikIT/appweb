1ed4f982af17e33a28b296adec8c4969f3ed99df
GPAlexander <gpfordevoloper@gmail.com>
2024-08-02 14:01:33 -0600

    Add EmailService for dynamic email sending with attachments

---
+++ app/Services/EmailService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class EmailService
+{
+    public function sendEmail($recipient, $subject, $view, $data = [], $attachments = [], $fromEmail = 'noreply@bclinik.com', $fromName = 'Calculadora IMC', $replyToEmail = 'noreply@bclinik.com', $replyToName = 'Calculadora IMC')
+    {
+        $mail = new PHPMailer(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->isSMTP();
+        $mail->Host = 'bclinik.com';
+        $mail->Port = 465;
+        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'noreply@bclinik.com';
+        $mail->Password = 'Password$001';
+        $mail->setFrom($fromEmail, $fromName);
+        $mail->addReplyTo($replyToEmail, $replyToName);
+        $mail->addAddress($recipient);
+
+        $mail->Subject = $subject;
+        $mail->isHTML(true);
+        $mail->MsgHTML(view($view, $data)->render());
+
+        foreach ($attachments as $attachment) {
+            if (is_array($attachment)) {
+                $mail->addStringAttachment($attachment['content'], $attachment['name']);
+            } else {
+                $mail->addAttachment($attachment);
+            }
+        }
+
+        if (!$mail->send()) {
+            throw new \Exception('Error al enviar el correo: ' . $mail->ErrorInfo);
+        }
+    }
+}
